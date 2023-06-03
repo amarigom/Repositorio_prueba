3f203e89704b027dfc2e77a750624306752a10e2
amarigom <andreamarigomez@gmail.com>
2023-06-03 18:46:19 -0300

    segundo commit creacion de componentes A, B y clase Persona

---
+++ src/components/componenteA.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function componenteA(apellido) {
+  return (
+    <div>
+      <button  ></button>
+    </div>
+  )
+}
+
+componenteA.propTypes = {
+
+}
+
+export default componenteA
+
---
+++ src/models/persona.class.js
@@ -0,0 +1,11 @@
+export  class Persona{
+
+    nombre='';
+    apellido='';
+
+
+    constructor(nombre,apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+}
